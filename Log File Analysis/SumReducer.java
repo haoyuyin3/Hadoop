905648d5ebd5d7cf644f444f2d8c509ed97a8c68
isaacyin12345 <37584977+isaacyin12345@users.noreply.github.com>
2019-10-13 10:29:13 -0400

    Add files via upload

---
+++ Log File Analysis/SumReducer.java
@@ -0,0 +1,23 @@
+package stubs;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+	  @Override
+	  public void reduce(Text key, Iterable<IntWritable> values, Context context)
+	      throws IOException, InterruptedException {
+		  
+	    int word_count = 0;
+	    
+	    for (IntWritable value : values) {
+	    	word_count = word_count + value.get();
+	    }
+	    
+	    context.write(key, new IntWritable(word_count));
+	  }
+	}
